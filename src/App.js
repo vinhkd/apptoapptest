e4ebad0d91d1c50e591a68b22be687003c645ea5
Vinh Nguyen <vnguyen@paramountcommerce.com>
2022-05-13 23:11:29 +0700

    push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe src='http://localhost:3000' width={1000} height={800} />
+    </div>
+  );
+}
+
+export default App;
